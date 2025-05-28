0f6032f949ddb4e2068daa54a210e37f3972ff71
Elviskiprono <biggeelvo831@gmail.com>
2025-05-28 12:17:08 +0300

    Modified

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
-# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
 engine = create_engine("sqlite:///habits.db")
 Session = sessionmaker(bind=engine)
-session = Session()
+
+def get_session():
+    return Session()
 
 def init_db():
     Base.metadata.create_all(engine)
